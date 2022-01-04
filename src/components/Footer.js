e626f379359c3a40d2f411b20c94e99ca561f216
unknown <jonydarakdjian@gmail.com>
2022-01-03 21:17:22 -0300

    v.1

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = ({date}) => (
+     
+        <footer>
+            <p>Todos los derechos reservados &copy; {date}</p>
+        </footer>
+     );
+
+ 
+export default Footer;
